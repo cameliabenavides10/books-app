d96ed82dffb68a4a8a77e7818716abb3482fd7ba
HoldyClaus <holdenclaus0212@gmail.com>
2023-02-10 11:38:11 -0600

    databases

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const routes = require('./controllers');
+// const routes = require('./controllers');
 // const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
