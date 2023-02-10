0fc79d80fdbf7ad9ee88e937b67b2946f56d496a
Harrison Boatman <hrry.boatman2@gmail.com>
2023-02-10 15:00:32 -0600

    started bookroutes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -6,12 +6,12 @@ router.post('/', async (req, res) => {
     try {
       const userData = await User.create(req.body);
   
-    //   req.session.save(() => {
-    //     req.session.user_id = userData.id;
-    //     req.session.logged_in = true;
+      req.session.save(() => {
+        req.session.user_id = userData.id;
+        req.session.logged_in = true;
   
         res.status(200).json(userData);
-      // });
+      });
     } catch (err) {
       res.status(400).json(err);
     }
--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use(routes);
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
