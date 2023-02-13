8c3a56fb253a6b5054c1580154174ecf5fb4e43e
HoldyClaus <holdenclaus0212@gmail.com>
2023-02-13 10:46:17 -0600

    attempt at login

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const helpers = require('./utils/helpers');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
