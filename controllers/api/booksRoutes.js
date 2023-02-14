a5f25057be82526372c755ddf99b7d54353618a0
Harrison Boatman <hrry.boatman2@gmail.com>
2023-02-13 23:18:58 -0600

    functional book search not functional DB post method

--- controllers/api/booksRoutes.js
+++ controllers/api/booksRoutes.js
@@ -3,7 +3,7 @@ const { Book } = require('../../models');
 const axios = require('axios')
 const withAuth = require('../../utils/auth');
 
-
+var bookList = []
 
 // to create a new book to the database
 //post('/:index');
@@ -30,7 +30,7 @@ router.post('/:index', withAuth, async (req, res) => {
 router.post('/', withAuth, async (req, res) => {
   try {
     const newBook = await Book.create({
-      // ...req.body,
+      ...req.body,
 
       reader_id: req.session.user_id,
     });
@@ -66,59 +66,63 @@ router.delete('/:id', withAuth, async (req, res) => {
 
 
 
-router.get('/booksearch/:searchTerm', withAuth, async (req, res) => {
-  let bookList = []
-  // var titleSearch = 'mistborn'
-  // var authorSearch = 'Brandon sanderson';
-  // authorSearch = authorSearch.split(" ").join('%20')
-  // var genreSearch;
-  // var searchSelector = 'title';
-  // var searchTerm
+// router.get('/booksearch/:searchTerm', withAuth, async (req, res) => {
+  
+//   // var titleSearch = 'mistborn'
+//   // var authorSearch = 'Brandon sanderson';
+//   // authorSearch = authorSearch.split(" ").join('%20')
+//   // var genreSearch;
+//   // var searchSelector = 'title';
+//   // var searchTerm
 
 
-  // if (searchSelector == 'genre') {
-  //   searchTerm = genreSearch
-  // } else if (searchSelector == 'author') {
-  //   searchTerm = 'inauthor:' + authorSearch;
-  // } else if (searchSelector == 'title') {
-  //   searchTerm = titleSearch;
-  // }
-  const bookURL = `https://www.googleapis.com/books/v1/volumes?q=${req.params.searchTerm}&maxResults=6&key=AIzaSyD7Dwq_e3cP_InmvZFjC5IJcefiw-bXM8s`
+//   // if (searchSelector == 'genre') {
+//   //   searchTerm = genreSearch
+//   // } else if (searchSelector == 'author') {
+//   //   searchTerm = 'inauthor:' + authorSearch;
+//   // } else if (searchSelector == 'title') {
+//   //   searchTerm = titleSearch;
+//   // }
+//   const bookURL = `https://www.googleapis.com/books/v1/volumes?q=${req.params.searchTerm}&maxResults=6&key=AIzaSyD7Dwq_e3cP_InmvZFjC5IJcefiw-bXM8s`
+//   bookList = []
+//   const bookData = await axios.get(bookURL, {
+//     params: {
+//       per_page: 3
+//     }
+//   });
 
-  const bookData = await axios.get(bookURL, {
-    params: {
-      per_page: 3
-    }
-  });
-
-  console.log(bookURL)
-  console.log(bookData.data.items[0])
-  const bookCount = Math.min(bookData.data.items.length, 5);
-  for (let i = 0; i < bookCount; i++) {
-    bookList.push({
-      title: bookData.data.items[i].volumeInfo.title,
-      description: bookData.data.items[i].volumeInfo.description,
-      authors: bookData.data.items[i].volumeInfo.authors,
-      thumbnail: bookData.data.items[i].volumeInfo.imageLinks.smallThumbnail
-    });
-  };
-  req.session.save(() => {
-    req.session.bookData = bookData.data;
-    req.session.logged_in = true;
-    req.session.books = [];
+//   console.log(bookURL)
+//   console.log(bookData.data.items[0])
+//   const bookCount = Math.min(bookData.data.items.length, 5);
+//   for (let i = 0; i < bookCount; i++) {
+//     bookList.push({
+//       title: bookData.data.items[i].volumeInfo.title,
+//       description: bookData.data.items[i].volumeInfo.description,
+//       authors: bookData.data.items[i].volumeInfo.authors,
+//       thumbnail: bookData.data.items[i].volumeInfo.imageLinks.smallThumbnail
+//     });
+//   };
+//   req.session.save(() => {
+//     req.session.bookData = bookData.data;
+//     req.session.logged_in = true;
+//     req.session.books = [];
 
 
-    // for (let i = 0; i < bookCount; i++) {
-    //   bookList.push(bookData.data.items[i]);
-    // };
-  });
+//     // for (let i = 0; i < bookCount; i++) {
+//     //   bookList.push(bookData.data.items[i]);
+//     // };
+//   });
 
-  // res.redirect('/search')
-  res.render('search', { bookList })
-  // return res.send(bookList)
-});
+//   res.redirect('/api/books/results')
+//   // res.render('search', { bookList })
+//   // return res.send(bookList)
+//   // return res.send(bookData.data.items)
+// });
 
-// router.get('/results')
+
+// router.get('/results', async (req, res)=> {
+//   res.render('search', {bookList})
+// })
 
 module.exports = router
 
