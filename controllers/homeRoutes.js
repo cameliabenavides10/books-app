25a66f2b005a33a12ad7dfe933629d46dfc3fc5b
cameliabenavides10 <cameliabenavides@gmail.com>
2023-02-14 14:53:28 -0600

    functioning search on recs page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -70,7 +70,7 @@ router.get('/library', (req, res) => {
 
 
 // Use withAuth middleware to prevent access to route
-router.get('/profile', withAuth, async (req, res) => {
+router.get('/profile', async (req, res) => {
   try {
     // Find the logged in user based on the session ID
     const userData = await User.findByPk(req.session.user_id, {
